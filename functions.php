c345701b6842b6448710a9bc6408be1397bf328b
gonzo47 <gonzalez.benjamin47@gmail.com>
2018-11-10 17:18:53 +0100

    ajout fonction entreprise ou client

---
+++ functions.php
@@ -0,0 +1,23 @@
+<?php
+require_once('../includes/Models/devis.php');
+	require_once('../includes/Models/client.php');
+
+function checkInput($data) 
+{
+	$data = trim($data);
+	$data = stripslashes($data);
+	$data = htmlspecialchars($data);
+	return $data;
+}
+
+
+function identiteClient($ent,$part){
+	if(!empty($ent)){
+		$result = print '<td>'. $ent .'</td>';
+	}
+	else{
+		$result = print '<td>'.$part.'</td>';
+	}
+	return $result;
+}
+?>

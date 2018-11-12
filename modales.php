d51b98f356fe229180b6dccd83ce321377dccdab
RoseReaper <elodie.changey@laposte.net>
2018-11-12 12:29:11 +0100

    Fusion modales

--- modales.php
+++ modales.php
@@ -1,10 +1,10 @@
-<!--Ajouter un produit-->
+<!--Ajouter / Modifier un produit-->
 
-<div id="modal_ajouter_produit" class="modal">
+<div id="modaleProduit" class="modal">
   <div class="modal-content">
   <div class="modal-header">
     <span class="close">&times;</span>
-    <h2>Ajouter un produit</h2>
+    <h2 class="titreModale"></h2>
   </div>
   <div class="modal-body">
     <form>
@@ -40,53 +40,6 @@
         <input type="text" id="newCatégorie" name="newCatégorie">
         <button type="button" id="validerNewCatégorie" name="validerNewCatégorie">Valider</button>
       </select></p>
-
-      <p>Cocher cette case pour désactiver le produit :<br>
-      <input type="checkbox" name="inactif" id="inactif" /> <label for="inactif"></label></p>
-
-     <button type="button">Valider</button>
-     <button type="button">Annuler</button>
-  </div>
-  </div>
-</div>
-
-<!--Modifier un produit-->
-
-<div id="modal_modif_produit" class="modal">
-  <div class="modal-content">
-  <div class="modal-header">
-    <span class="close">&times;</span>
-    <h2>Modifier la Fiche produit</h2>
-  </div>
-  <div class="modal-body">
-    <form>
-      <p><label for="libellé_produit">Libellé :</label>
-      <input type="text" id="libellé_produit" name="libellé_produit"></p>
-
-      <p><label for="marque_produit">Marque :</label>
-      <select name="marque_produit" id="marque_produit">
-        <option value="0" id="no">--</option>
-        <option value="1" id="ok">Nouveau</option>
-      </select></p>
-
-      <p><label for="gamme_produit">Gamme :</label>
-      <select name="gamme_produit" id="gamme_produit">
-        <option value="0">--</option>
-      </select></p>
-
-      <p><label for="référence_produit">Référence :</label>
-      <input type="text" id="référence_produit" name="référence_produit"></p>
-
-      <p><label for="prixht_produit">Prix (HT) :</label>
-      <input type="text" id="prixht_produit" name="prixht_produit"></p>
-
-      <p><label for="tva_produit">TVA :</label>
-      <select name="tva_produit" id="tva_produit">
-        <option value="0">--</option>
-      </select></p>
-
-      <p><label for="quantité_produit">Quantité :</label>
-      <input type="text" id="quantité_produit" name="quantité_produit"></p>
 
       <p>Cocher cette case pour désactiver le produit :<br>
       <input type="checkbox" name="inactif" id="inactif" /> <label for="inactif"></label></p>
--- produit/index.php
+++ produit/index.php
@@ -1,6 +1,5 @@
 <?php
 	require_once('../config.php');
-	require '../modales.php';
 
 	$db = Database::connect();
 
@@ -27,7 +26,9 @@
     <body>
         <div class="wrapper">
 
-			<?php include('../nav.php'); ?>
+			<?php include('../nav.php');
+require_once('../modales.php');
+			?>
 
 			<section>
 
@@ -58,7 +59,7 @@
 									print '<td>' . $produit->nomM . '</td>';
 									print '<td>' . $produit->libelleG . '</td>';
 									print '<td>' . $produit->libelleC . '</td>';
-									print '<td><img class="modal_modif" src="../includes/assets/pencil.png" title="Modifier Produit" alt="bouton_modifier" height="20">
+									print '<td><img class="boutonAppel" src="../includes/assets/pencil.png" title="Modifier Produit" alt="bouton_modifier" height="20">
 						          <a href="#"><img src="../includes/assets/cancel.png" title="Supprimer Produit" alt="bouton_supprimer" height="20"/></a></td>';
 								print '</tr>';
 							}
@@ -72,37 +73,33 @@
 
 				<script type="text/javascript">
 
-				var modal_modif = document.getElementById("modal_modif_produit");
-				var btn_modif = document.getElementsByClassName("modal_modif");
-				for(i=0; i<btn_modif.length; i++){
-				  btn_modif[i].onclick = function() {
-				    modal_modif.style.display = "block";
-				  }
-				}
+				var modaleGen = document.getElementById("modaleProduit");
 
-				var modal_ajout = document.getElementById("modal_ajouter_produit");
-				var btn_ajout = document.getElementById("bouton_ajouter");
-				btn_ajout.onclick = function() {
-				  modal_ajout.style.display = "block";
+				function displayModal(){
+					modaleGen.style.display = "block";
 				}
 
 				var span_ajout = document.getElementsByClassName("close")[0];
-				var span_modif = document.getElementsByClassName("close")[1];
 
 				span_ajout.onclick = function() {
-				  modal_ajout.style.display = "none";
-				}
-
-				span_modif.onclick = function() {
-				  modal_modif.style.display = "none";
+				  modaleGen.style.display = "none";
 				}
 
 				window.onclick = function(event) {
-				  if (event.target == modal) {
-				    modal_ajout.style.display = "none";
-				    modal_modif.style.display = "none";
+				  if (event.target == modaleGen) {
+				    modaleGen.style.display = "none";
 				  }
 				}
+
+				$(".boutonAppel").on('click', function(){
+						$(".titreModale").text('Modifier la fiche Produit');
+						displayModal();
+					});
+
+				$("#bouton_ajouter").on('click', function(){
+						$(".titreModale").text('Ajouter un Produit');
+						displayModal();
+					});
 
 				$(document).ready( function () {
 				    $('#table_produits').DataTable();

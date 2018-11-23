19e00aff1d064ea16cc87d3a0f2e5c4196b2b654
maxime173 <17.laurinmaxime@gmail.com>
2018-11-23 15:16:16 +0100

    Modifications client

--- clients/index.php
+++ clients/index.php
@@ -10,9 +10,21 @@ $db = Database::connect();
 $statement = $db->query(' SELECT client.id_client as id_client,
 			client.code_client as code,
             client.raison_sociale as raisonsoc,
-			civilite.libelle as civilite, contact.nom as nom_cli,contact.prenom as prenom_cli, client.actif,
-            contact.service as service, adresse.ligne1 as ligne1, adresse.ligne2 as ligne2,liste_adresse.libelle as libelle_adresse,
-            ville.cp_ville as code_postal, ville.nom_ville as nom_ville
+            client.actif,
+            client.id_client,
+            client.id_adresse_facturation,
+			civilite.libelle as civilite,
+            civilite.id_civilite as idcivilite,
+            contact.id_contact,
+            contact.nom as nom_cli,
+            contact.prenom as prenom_cli,
+            contact.service as service,
+            adresse.ligne1 as ligne1,
+            adresse.ligne2 as ligne2,
+            liste_adresse.libelle as libelle_adresse,
+            ville.id_ville,
+            ville.cp_ville as code_postal,
+            ville.nom_ville as nom_ville
             FROM contact
             inner JOIN client ON client.id_client = contact.id_client
             inner join liste_adresse on liste_adresse.id_client = client.id_client
@@ -50,7 +62,7 @@ Database::disconnect();
                     <tr>
                         <th>Code client</th>
                         <th>Raison sociale</th>
-                        <th colspan="2">Actions</th>
+                        <th></th><th></th>
                         <th>Actif?</th>
                     </tr>
                 </thead>
@@ -61,8 +73,8 @@ Database::disconnect();
                        // $script .= '<tr data-id="'.$client->id_client.'">';
                             print '<td>'.$client->code.'</td>';
                             print '<td>'.identiteClient($client->raisonsoc,$client->civilite." ".$client->prenom_cli." ".$client->nom_cli).'</td>';
-                            print '<td><img class="boutonAppel" <img class="boutonAppel" data-code="'.$client->code.'" data-raison_sociale="'.$client->raisonsoc.'" data-nom_client="'.$client->nom_cli.'"data-prenom_client="'.$client->prenom_cli.'" data-civilite="'.$client->civilite.'" data-ligne1="'.$client->ligne1.'"data-service="'.$client->service.'" data-ligne1="'.$client->ligne1.'"
-     data-ligne2="'.$client->ligne2.'" data-libelle_adresse="'.$client->libelle_adresse.'" data-code_postal="'.$client->code_postal.'"
+                            print '<td><img class="boutonAppel" <img class="boutonAppel" data-code="'.$client->code.'" data-raison_sociale="'.$client->raisonsoc.'" data-clientActif="'.$client->actif.'" data-nom_client="'.$client->nom_cli.'"data-prenom_client="'.$client->prenom_cli.'" data-civilite="'.$client->idcivilite.'" data-ligne1="'.$client->ligne1.'"data-service="'.$client->service.'" data-ligne1="'.$client->ligne1.'"
+     data-ligne2="'.$client->ligne2.'" data-libelle_adresse="'.$client->libelle_adresse.'" data-code_postal="'.$client->code_postal.'"data-id_contact="'.$client->id_contact.'"data-id_ville="'.$client->id_ville.'"data-id_adresse_facturation="'.$client->id_adresse_facturation.'"data-id_client="'.$client->id_client.'"
      data-nom_ville="'.$client->nom_ville.'" src="../includes/assets/pencil.png" title="Modifier un Client" alt="bouton_modifier" height="20"></td>
 
 
@@ -110,7 +122,7 @@ Database::disconnect();
                 }
 
                 var closeModal = document.getElementsByClassName("close")[0];
-                var cancelModal = document.getElementById("btnAnnuler");
+                var cancelModal = document.getElementById("btnAnnulerClient");
 
               closeModal.onclick = function() {
 				  modaleGen.style.display = "none";
@@ -121,18 +133,8 @@ Database::disconnect();
 				}
 
 
-                $("input:checkbox").on("change", function() {
-                    var xhr;
-                    if (window.XMLHttpRequest)
-                        xhr = new XMLHttpRequest();
-                    else
-                        xhr = new ActiveXObject("microsoft.Xmlhttp");
-                    xhr.onreadystatechange = function() {
-                        if (xhr.readyState == 4) {
-                            var retourAjax = xhr.responseText;
-                            console.log(retourAjax);
-                        }
-                    }
+                $("#table_client input:checkbox").on("change", function()
+                {
                     alert("Vous changez le statut de ce client");
                     var id_client = $(this).data("id");
                     console.log(id_client);
@@ -140,21 +142,14 @@ Database::disconnect();
                     console.log(val);
                     var apply = $(this).is(':checked') ? true : false;
                     console.log(apply);
-                    //var data = "id_client=" + id_client + "&apply=" + apply;
-                    //xhr.open('post', 'ajax.php', true);
-                    //xhr.setRequestHeader('content-type', 'application/x-www-form-urlencoded; charset=utf-8');
-                    //xhr.send(data);
-
-
-
                     $.ajax({
                         type: "POST",
                         url: "ajax.php",
                         data: {
                             id_client: id_client,
                             val: val,
-                            apply: apply //,
-                            //action: "changerActif"
+                            apply: apply ,
+                            action: "changerActif"
                         }
 
                     });
@@ -163,32 +158,42 @@ Database::disconnect();
 
 
                 $(".boutonAppel").on('click', function() {
+                    $("#btnAjouterClient").hide();
+                    $("#btnModifierClient").show();
                     $(".titreModale").text('Modifier la fiche client');
+                    $("#id_client").val($(this).data("id_client"));
                     $("#code_client").val($(this).data("code"));
+                    $("#code_client").prop("disabled", true);
                     $("#raison_sociale").val($(this).data("raison_sociale"));
                     $("#service").val($(this).data("service"));
-                    $("#civilite").val($(this).data("civilite"));
+                    $('input[name=gender][value='+ $(this).data("civilite") +']').prop('checked', 'checked');
+                    $("#id_contact").val($(this).data("id_contact"));
                     $("#nom").val($(this).data("nom_client"));
                     $("#prenom").val($(this).data("prenom_client"));
-                     $("#nomAdresse").val($(this).data("libelle_adresse"));
-                    $("#id_adresse_facturation").val($(this).data("ligne1"));
-
-                     $("#id_adresse_comp").val($(this).data("ligne2"));
-                    $("#postale").val($(this).data("code_postal"));
+                    $("#id_adresse_facturation").val($(this).data("id_adresse_facturation"));
+                    $("#nomAdresse").val($(this).data("libelle_adresse"));
+                    $("#ligne1").val($(this).data("ligne1"));
+                    $("#ligne2").val($(this).data("ligne2"));
+                    $("#cPostale").val($(this).data("code_postal"));
                     $("#ville").val($(this).data("nom_ville"));
+                    $("#id_ville").val($(this).data("id_ville"));
+                    $('#clientActif').prop('checked', $(this).data("clientActif") == 1);
                     displayModal();
                 });
                 $("#bouton_ajouter").on('click', function() {
+                    $("#btnAjouterClient").show();
+                    $("#btnModifierClient").hide();
                     $(".titreModale").text('Ajouter un Client');
-                    $("#code").val("");
-                    $("#raison_sociale").val("lol");
+                    $("#code_client").val("");
+                    $("#code_client").prop("disabled", false);
+                    $("#raison_sociale").val("");
                     $("#civilite").val("");
-                    $("#nom").val(0);
-                    $("#prenom").val(0);
-                     $("#service").val(0);
-                     $("#id_adresse_facturation").val(0);
-                     $("#id_adresse_comp").val(0);
-                     $("#nomAdresse").val(0);
+                    $("#nom").val("");
+                    $("#prenom").val("");
+                    $("#service").val(0);
+                    $("#id_adresse_facturation").val(0);
+                    $("#id_adresse_comp").val(0);
+                    $("#nomAdresse").val(0);
                     $("#postale").val(0);
                     $("#ville").val(0);
                     displayModal();

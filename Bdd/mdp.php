e741e8c3c3f6f3129b6cf55d9edb32b9316f0d37
gonzo47 <gonzalez.benjamin47@gmail.com>
2018-11-23 16:32:27 +0100

    f

--- Bdd/mdp.php
+++ Bdd/mdp.php
@@ -1,2 +1,18 @@
 <?php
-define("MDPBDD", "root");
+
+function creerIdentifiant()
+{
+	$hote = 'localhost';
+	$bdd = 'progiplus';
+	$dbUser = 'root';
+	$dbMdp = 'Rugbyman47';
+
+
+	define("DSN_BDD", 'mysql:host='.$hote.';dbname='.$bdd);
+	define("USER_BDD", $dbUser);
+	define("MDP_BDD", $dbMdp);
+}
+
+creerIdentifiant();
+
+?>

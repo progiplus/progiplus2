edf06dd2f2ad90ea60d52bc489b08fbe12a9ee67
maxime173 <17.laurinmaxime@gmail.com>
2018-11-27 14:44:59 +0100

    Modale Contact

--- Bdd/mdp.php
+++ Bdd/mdp.php
@@ -5,12 +5,12 @@ function creerIdentifiant()
 	$hote = 'localhost';
 	$bdd = 'progiplus';
 	$dbUser = 'root';
-	$dbMdp = 'root';
+	$dbMdp = 'azertysio';
 	$port = "3306";
 	
 	define("DSN_BDD", 'mysql:host='.$hote.';port='.$port.';dbname='.$bdd);
-	define("USER_BDD", $dbMdp);
-	define("MDP_BDD", $dbUser);
+	define("USER_BDD", $dbUser);
+	define("MDP_BDD", $dbMdp);
 }
 
 creerIdentifiant();

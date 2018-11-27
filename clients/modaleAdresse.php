dee46f59dc187b6c10db37900c30a1ca2b718109
Bouchet Kateryna <katerynabouchet7@gmail.com>
2018-11-27 15:51:54 +0100

    ajout modale adresse

---
+++ clients/modaleAdresse.php
@@ -0,0 +1,42 @@
+<div id="modaleClient" class="modal">
+    <div class="modal-content">
+        <div class="modal-header">
+            <span class="close">&times;</span>
+            <h2 class= "titreModale">Ajout adresse et modification adresse facturation</h2>
+        </div>
+        <div class="modal-body">
+           <br>
+            <div id="container">
+                <div class="element"><label for="adresseFac">Adresse de facturation :</label>
+                             <select  id="adresseFac" name="adresseFac" required value="">
+                                 
+                             </select></div><br>
+
+
+                        <div class="element"><label for="nomAdresse">Nom adresse :</label>
+                            <input id="nomAdresse" name="nomAdresse" required value=""></div><br>
+
+
+                        <div class="element"> <label for="ligne1">Ligne1 :</label>
+                            <input id="ligne1" name="ligne1" value=""></div><br>
+                            
+                       <div class="element"> <label for="ligne2">Ligne2 :</label>
+                            <input id="ligne2" name="ligne2" value=""></div><br>
+
+
+                        <div class="element"> <label for="cp">Code postal :</label>
+                            <input id="cp" name="cp" value=""></div><br>
+
+
+
+                        <div class="element"><label for="ville">Ville:</label>
+                            <input id="ville" name="ville" value=""></div> <br>
+
+
+                        <div class="element">
+                            <input class="button" type="button" value="Valider" id="">
+                            <input class="button" type="reset" value="Annuler"></div><br>
+            </div>
+    </div>
+ </div>
+</div>
